9422ad06ec3d62bd8e233ca9eb7179ca4f1e096e
Jinu Joy <jinuparekatil@gmail.com>
2025-11-27 16:38:21 +0530

    Added news repository for fetch the data

---
+++ lib/data/repositories/newsrepository.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_reader_app/data/models/newsmodel.dart';
+
+class Newsrepository {
+  getNews(String category, String country) async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=d346389fdafc404ea07a4068f1738fd1";
+    Uri uri = Uri.parse(url);
+    try {
+      http.Response response = await http.get(uri);
+      return NewsResponse.fromJson(jsonDecode(response.body));
+    } catch (e) {
+      print("Error fetching news");
+      return NewsResponse.showError(e.toString());
+    }
+  }
+}
