88070010e09e0971bf92a8fdf9c1672b585db07d
Jinu Joy <jinuparekatil@gmail.com>
2025-11-27 18:11:06 +0530

    Added bloc

---
+++ lib/features/home/bloc/get_news_bloc.dart
@@ -0,0 +1,19 @@
+import 'package:news_reader_app/data/models/newsmodel.dart';
+import 'package:news_reader_app/data/repositories/newsrepository.dart';
+import 'package:rxdart/subjects.dart';
+
+class GetNewsBloc {
+  final Newsrepository newsrepository = Newsrepository();
+  final BehaviorSubject<NewsResponse> _subject =
+      BehaviorSubject<NewsResponse>();
+  BehaviorSubject<NewsResponse> get subject => _subject;
+
+  getNews(String category, String country) async {
+    NewsResponse newsResponse = await newsrepository.getNews(category, country);
+    _subject.sink.add(newsResponse);
+  }
+
+  dispose() {
+    _subject.close();
+  }
+}
